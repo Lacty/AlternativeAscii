cd55bde1ae1c4286f1709de1ca401181ba4a15fa
Lacty <akira206@gmail.com>
2016-11-20 19:05:09 +0900

    add user.xml
    xmlからウィンドウのサイズやフレームレートを指定できるようにした

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofxXmlSettings.h"
 
 
 int main( ){
-	ofSetupOpenGL(1024, 768, OF_FULLSCREEN);
+  ofxXmlSettings xml;
+  xml.loadFile("user.xml");
+  xml.pushTag("Window");
+  
+  int  w      = xml.getValue("Width", 1024);
+  int  h      = xml.getValue("Height", 768);
+  bool fullSc = xml.getValue("FullScreen", 0);
+  int  fps    = xml.getValue("FrameRate", 60);
+  
+	ofSetupOpenGL(w, h, fullSc ? OF_FULLSCREEN : OF_WINDOW);
+  ofSetFrameRate(fps);
+  
 	ofRunApp(new ofApp());
 }
