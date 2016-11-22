0f0d92caf23ea2a74ecbb21c1fdf3b1b6981fffd
Lacty <akira206@gmail.com>
2016-11-22 23:38:16 +0900

    fix confrict

--- src/player/jumpingState.cpp
+++ src/player/jumpingState.cpp
@@ -11,17 +11,17 @@ shared_ptr<PlayerState> JumpingState::handleInput(Player& player, ofxJoystick& i
     // 着地していたら前の状態に繊維させる
     return PlayerState::finish;
   }
-  
+
   return nullptr;
 }
 
 void JumpingState::update(Player& player, ofxJoystick& input) {
   // ジャンプ中でも移動状態を更新する
   MovingState::update(player, input);
-  
+
   // 暫定 ベクトルを使用したジャンプの処理
   ofVec2f newVel = player.getVel();
-  
+
   newVel.y -= 0.8;
   ofVec2f newLocation = player.getPos() + newVel;
   if (newLocation.y <= FLOOR) {
