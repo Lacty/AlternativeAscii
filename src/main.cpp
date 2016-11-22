c148997e2c5e46f790475fe21b75a253dca8b885
Lacty <akira206@gmail.com>
2016-11-22 20:24:07 +0900

    他の設定も読み込むのでxml_.pushTagを消した

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,11 @@
 int main( ){
   ofxXmlSettings xml;
   xml.loadFile("main.xml");
-  xml.pushTag("Window");
   
-  int  w      = xml.getValue("Width", 1024);
-  int  h      = xml.getValue("Height", 768);
-  bool fullSc = xml.getValue("FullScreen", 0);
-  int  fps    = xml.getValue("FrameRate", 60);
+  int  w      = xml.getValue("Window:Width",    1024);
+  int  h      = xml.getValue("Window:Height",    768);
+  bool fullSc = xml.getValue("Window:FullScreen",  0);
+  int  fps    = xml.getValue("Window:FrameRate",  60);
   
 	ofSetupOpenGL(w, h, fullSc ? OF_FULLSCREEN : OF_WINDOW);
   ofSetFrameRate(fps);
