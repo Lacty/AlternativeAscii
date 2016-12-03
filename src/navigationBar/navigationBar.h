b4eca101606139025d03644f2302d2f4e776164e
Naoto Fujiwara <asyura1327@yahoo.co.jp>
2016-12-03 11:54:06 +0900

    update

---
+++ src/navigationBar/navigationBar.h
@@ -0,0 +1,68 @@
+
+#pragma once
+
+
+#include "ofMain.h"
+#include "ofxXmlSettings.h"
+#include "ofxImGui.h"
+
+
+class NavigationBar {
+private:
+  ofxXmlSettings xml_;
+
+  //テキスト
+  ofTrueTypeFont font_;
+	vector<string> textList_;
+  ofVec2f textPos_;
+  ofVec2f defaultPos_;
+	int count_;
+	int waitTime_;
+	int textSwitch_;
+	int moveSwitch_;
+	float moveValue_;
+
+  //画像
+  string mainBarFilename_;
+  string sideBarFilename_;
+  ofImage mainBarImage_;
+  ofImage sideBarImage_;
+  ofVec2f barPos_;
+  ofVec2f barSize_;
+
+  //フォント
+  string fontFilename_;
+  float fontScale_;
+  float fontSize_;
+
+
+  void loadFiles();
+	void saveXml();
+  void loadXml();
+  void loadFont();
+  void moveText(ofEventArgs &args);
+  void frameOutTextStart();
+  void frameOutTextEnd();
+  void frameInTextStart();
+  void frameInTextEnd();
+  void moveTextPos();
+  void drawSwitchText();
+  void drawText(string& text);
+
+public:
+  void setup();
+  void draw();
+  void drawGui();
+
+
+  void setTexPos(ofVec2f& pos) { textPos_ = pos; }
+  ofVec2f getTexPos() { return textPos_; }
+  void setDefaultPos(ofVec2f& pos) { defaultPos_ = pos; }
+  ofVec2f setDefaultPos() { return defaultPos_; }
+  void setFontSize(float size) { fontSize_ = size; }
+  float getFontSize() { return fontSize_; }
+  void setWaitTime(int time) { waitTime_ = time; }
+  int getWaitTime() { return waitTime_; }
+  void setMoveValue(float value) { moveValue_ = value; }
+  float getMoveValue() { return moveValue_; }
+};
