8d4993698ab98f49e26da9ce4be4bed6dfd92009
Lacty <akira206@gmail.com>
2016-12-09 19:58:29 +0900

    SeekBar 追加

--- src/seekBar/seekBar.cpp
+++ src/seekBar/seekBar.cpp
@@ -1,9 +1,77 @@
-//
-//  seekBar.cpp
-//  AlternativeAscii
-//
-//  Created by Yanai on 2016/12/09.
-//
-//
 
-#include "seekBar.hpp"
+#include "seekBar.h"
+
+
+void SeekBar::setup(const string& path) {
+  ofxJSON json;
+  json.open(path);
+  
+  height_   = json["Root"]["height"].asInt();
+  barWidth_ = json["Bar"]["width"].asInt();
+  
+  // -----------------------------------------------------------------
+  // Load Color //
+  for (int i = 0; i < 3; i++) {
+    rootColor_[i]   = json["Root"]["color"][i].asFloat();
+    seekedColor_[i] = json["Seeked"]["color"][i].asFloat();
+    barColor_[i]    = json["Bar"]["color"][i].asFloat();
+  }
+}
+
+void SeekBar::draw(int maxTime, int limitTime) {
+  int         halfBarWidth (barWidth_ * 0.5);
+  ofVec2f     barPos       ((ofGetWidth() - ((ofGetWidth() / maxTime) * limitTime)) - halfBarWidth,
+                            ofGetHeight() - height_);
+  ofVec2f     barSize      (barWidth_, height_);
+  
+  ofRectangle root         (0, ofGetHeight() - height_, ofGetWidth(), height_);
+  ofRectangle seeked       (0, ofGetHeight() - height_, barPos.x, height_);
+  ofRectangle bar          (barPos, barPos + barSize);
+
+  // -----------------------------------------------------------------
+  // ROOT
+  // -----------------------------------------------------------------
+  ofNoFill();
+  ofSetColor(100, 100, 100);
+  ofDrawRectangle(root);
+  
+  ofFill();
+  ofSetColor(rootColor_);
+  ofDrawRectangle(root);
+  
+  // -----------------------------------------------------------------
+  // SEEKED BAR
+  // -----------------------------------------------------------------
+  ofNoFill();
+  ofSetColor(100, 100, 100);
+  ofDrawRectangle(seeked);
+  
+  ofFill();
+  ofSetColor(seekedColor_);
+  ofDrawRectangle(seeked);
+  
+  // -----------------------------------------------------------------
+  // BAR
+  // -----------------------------------------------------------------
+  ofNoFill();
+  ofNoFill();
+  ofSetColor(100, 100, 100);
+  ofDrawRectangle(bar);
+  
+  ofFill();
+  ofSetColor(barColor_);
+  ofDrawRectangle(bar);
+}
+
+void SeekBar::drawParam() {
+  ImGui::Begin("SeekBar");
+  
+  ImGui::DragInt("Height", &height_);
+  ImGui::DragInt("Bar Width", &barWidth_);
+  
+  ImGui::ColorEdit3("Root Color", &rootColor_.r);
+  ImGui::ColorEdit3("Seeked Color", &seekedColor_.r);
+  ImGui::ColorEdit3("Bar Color", &barColor_.r);
+  
+  ImGui::End();
+}
--- src/seekBar/seekBar.h
+++ src/seekBar/seekBar.h
@@ -1,14 +1,23 @@
-//
-//  seekBar.hpp
-//  AlternativeAscii
-//
-//  Created by Yanai on 2016/12/09.
-//
-//
 
-#ifndef seekBar_hpp
-#define seekBar_hpp
+#pragma once
+#include "ofMain.h"
+#include "ofxImGui.h"
+#include "ofxJSONElement.h"
 
-#include <stdio.h>
 
-#endif /* seekBar_hpp */
+class SeekBar {
+private:
+  int height_;
+  int barWidth_;
+  
+  ofColor_<float> rootColor_;
+  ofColor_<float> seekedColor_;
+  ofColor_<float> barColor_;
+  
+public:
+  
+  void setup(const string& path);
+  
+  void draw(int maxTime, int limitTime);
+  void drawParam();
+};
